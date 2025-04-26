032deeaec927abb1114659126b910a43dc183b10
zamine-dots <zobaaamin@fsm.u-monastir.tn>
2025-04-26 09:59:46 -0700

    navbar update 2.2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import logo from '../img/logo.png';
-import '../navbar.css'; // Import the custom CSS file
+
 
 function Navbar() {
+  // Ensure Bootstrap JS is properly initialized
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min');
+  }, []);
+
   return (
-    <nav className="navbar navbar-expand-lg bg-light fixed-top">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/home">
-          <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
-        </a>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-top" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
+          aria-controls="navbarNav"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto ">
-            <li className="nav-item"><Link className="nav-link" to="/home">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
       </div>
